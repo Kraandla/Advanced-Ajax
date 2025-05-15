198ecb63f91c20c5b6de2a05647f28290c3b31a2
Kraandla <kevinrandla22@gmail.com>
2025-05-15 14:30:12 +0300

    Globale usage definition

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.ComponentModel;
+global using Microsoft.EntityFrameworkCore;
+global using Advanced_Ajax.Models;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.Rendering;
+global using Advanced_Ajax.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
