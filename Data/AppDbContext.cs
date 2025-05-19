7e43bc8898960b5766328e7a4e35646e569a69ac
Kraandla <kevinrandla22@gmail.com>
2025-05-19 15:54:02 +0300

    Data before reset

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Advanced_Ajax.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<City> Cities { get; set; }
+        public virtual DbSet<Country> Countries { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.ComponentModel;
+global using Microsoft.EntityFrameworkCore;
+global using Advanced_Ajax.Models;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.Rendering;
+global using Advanced_Ajax.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetSection("ConnectionStrings:DbConn").Value));
 
 var app = builder.Build();
 
