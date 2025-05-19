702dab82f8133de89f044df7a1d5dffdc7907ef3
Kraandla <kevinrandla22@gmail.com>
2025-05-19 15:56:59 +0300

    Create CountryController.cs

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Advanced_Ajax.Controllers
+{
+    public class CountryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
