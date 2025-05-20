dca65b16fe3a34634968ae7a31bcd81566296a17
Kraandla <kevinrandla22@gmail.com>
2025-05-20 17:49:37 +0300

    finish

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -112,5 +112,33 @@ namespace Advanced_Ajax.Controllers
             return lstCountries;
         }
 
+        [HttpGet]
+        public IActionResult CreateModalForm(int countryId)
+        {
+            City city = new City();
+            city.CountryId = countryId;
+            city.CountryName = GetCountryName(countryId); // Underlined, likely a warning or error
+            return PartialView("_CreateModalForm", city);
+        }
+
+        [HttpPost]
+        public IActionResult CreateModalForm(City city)
+        {
+            _context.Add(city);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        private string GetCountryName(int countryId)
+        {
+            if (countryId == 0)
+                return "";
+
+            string strCountryName = _context.Countries
+                .Where(ct => ct.Id == countryId)
+                .Select(nm => nm.Name).Single().ToString();
+
+            return strCountryName;
+        }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -42,8 +42,49 @@ document.getElementById('PhotoUrl').value = fileName;
 
 });
 
+
 function ShowCreateModalForm() {
     var myModal = new bootstrap.Modal(document.getElementById('DivCreateDialogholder'));
     myModal.show();
+    return;
+}
+
+function submitModalForm() {
+    var btnSubmit = document.getElementById('btnSubmit');
+    btnSubmit.click();
+}
+
+function refreshCountryList() {
+    var btnBack = document.getElementById('dupBackBtn');
+    btnBack.click();
+    FillCoutries("lstCountryId");
+}
+
+function refreshCityList() {
+    var btnBack = document.getElementById('dupBackBtn');
+    btnBack.click();
+    FillCities("lstCityId");
+}
+function FillCoutries(lstCountryId) { // Note the typo: FillCoutries
+    var lstCountries = $("#" + lstCountryId);
+    lstCountries.empty();
+
+    lstCountries.append($('<option />',
+        {
+            value: null,
+            text: "Select Country"
+        }));
+
+    $.getJSON("/country/GetCountries", function (countries) {
+        if (countries != null && !jQuery.isEmptyObject(countries)) {
+            $.each(countries, function (index, country) {
+                lstCountries.append($('<option/>',
+                    {
+                        value: country.value,
+                        text: country.text
+                    }));
+            });
+        };
+    });
     return;
 }
